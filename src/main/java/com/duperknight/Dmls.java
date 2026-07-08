37f54acbe4ce9ac759567b2515120da550ed3cde
DuperKnight <arthurtsmendes@gmail.com>
2026-07-08 22:07:00 +0100

    Initial commit

---
+++ src/main/java/com/duperknight/Dmls.java
@@ -0,0 +1,10 @@
+package com.duperknight;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dmls implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/duperknight/client/DmlsClient.java
@@ -0,0 +1,10 @@
+package com.duperknight.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DmlsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
